bf6244fd2a4e367f00254821b197963c6b710c0d
ragesalmon <ragesalmon@gmail.com>
2014-08-08 15:06:26 -0400

    Added some actual functionality to basic

--- basic/src/main.cpp
+++ basic/src/main.cpp
@@ -1,6 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <GL/glut.h>
+
+float angle = 0.0;
+
+
+void renderScene(void) {
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glLoadIdentity();
+
+  glBegin(GL_TRIANGLES);
+  glVertex3f(0.5, -0.5, 0);
+  glVertex3f(0.0, angle, 0.5);
+  glVertex3f(angle, 0.0, 0.5);
+  glEnd();
+
+  angle+=0.01;
+
+  glutSwapBuffers();
+}
 
 int main(int argc, char * argv[]) {
-  printf("Hello world");
-  return 0;
+
+  // init glut
+  glutInit(&argc, argv);
+  // Set display mode
+  glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+  // set window position
+  glutInitWindowPosition(50, 50);
+  // Set window size
+  glutInitWindowSize(500, 500);
+
+  // Create the window
+  glutCreateWindow("Lmaoooo");
+
+  // Set display function
+  glutDisplayFunc(renderScene);
+
+  // Set idle function
+  glutIdleFunc(renderScene);
+  
+  // call the main loop
+  glutMainLoop();
+  
+  return 1;
+
 }
